6ca8dbcc1b7f555e192e28c2884c201cd34d38a6
Cristiano Borges <cristaianovieirati@gmail.com>
2020-06-25 19:10:45 -0300

    Conclusao da API

--- app.js
+++ app.js
@@ -1,20 +1,61 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const app = express();
+const cors = require('cors');
+const swaggerJSDoc = require('swagger-jsdoc');
+const swaggerUI = require('swagger-ui-express');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const swaggerOptions = {
+    swaggerDefinition: {
+        info: {
+            title: "Shop API",
+            description: "Backend Api",
+            contact: {
+                name: 'Amazing Developer'
+            },
+            servers: "http://localhost:3636"
+        }
+    },
+    apis: ["app.js", ".routes/*.js"]
+};
 
-var app = express();
+const swaggerDocs = swaggerJSDoc(swaggerOptions);
+app.use('/api/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 
-app.use(logger('dev'));
+
+/* CORS */
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'PUT', 'DELETE', 'PATCH', 'POST'],
+    allowedHeaders: 'Content-Type, Authorization, Origin, X-Requested-With, Accept'
+}));
+app.use(logger('combined'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
+// Import Routes
+const usersRouter = require('./routes/users');
+const productsRouter = require('./routes/products');
+const authRouter = require('./routes/auth');
+const orderRouter = require('./routes/order');
+
+// Define Routes
+/**
+ * @swagger
+ * /api/products:
+ *   get:
+ *    description: Get All Products
+ *
+ */
+
+app.use('/api/users', usersRouter);
+app.use('/api/products', productsRouter);
+app.use('/api/auth', authRouter);
+app.use('/api/orders', orderRouter);
 
 module.exports = app;
